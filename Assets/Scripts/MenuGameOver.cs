acfcf662f52ccd087aea5635f03446db57676fe5
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 15:09:33 +0700

    add button function

---
+++ Assets/Scripts/MenuGameOver.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGameOver : MonoBehaviour
+{
+    public void Ulangi()
+    {
+        Debug.Log("Ulangi");
+    }
+
+    public void MenuUtama()
+    {
+        Debug.Log("Menu Utama");
+    }
+}
---
+++ Assets/Scripts/MenuUtama.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUtama : MonoBehaviour
+{
+    public void Mainkan()
+    {
+        Debug.Log("Mainkan");
+    }
+
+    public void Keluar()
+    {
+        Debug.Log("Keluar");
+    }
+}
