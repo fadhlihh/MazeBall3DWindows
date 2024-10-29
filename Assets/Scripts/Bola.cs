84e9b651b44b953da16b6304d01fe5e0315142ea
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 15:26:34 +0700

    add vfx

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -7,10 +7,15 @@ public class Bola : MonoBehaviour
     // Membuat variable untuk mereferensikan sfx bounce bola
     [SerializeField]
     AudioSource sfxBola;
+    // Membuat variable untuk mereferensikan vfx bola
+    [SerializeField]
+    ParticleSystem vfxBola;
 
     private void OnCollisionEnter(Collision other)
     {
         // Memutar audio sfx bounce
         sfxBola.Play();
+        // Memutar vfx bola
+        vfxBola.Play();
     }
 }
