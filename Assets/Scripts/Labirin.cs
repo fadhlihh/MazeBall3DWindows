c22478163dcea515d45d1250a911ccd878fd762e
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 11:46:12 +0700

    add execution flow

--- Assets/Scripts/Labirin.cs
+++ Assets/Scripts/Labirin.cs
@@ -4,54 +4,43 @@ using UnityEngine;
 
 public class Labirin : MonoBehaviour
 {
-    // Membuat variable nama dengan tipe string, dengan nilai awal "Agate"
-    [SerializeField]
-    string nama = "Agate";
-    // Membuat variable kecepatan dengan tipe float, dengan nilai awal "10.5"
-    [SerializeField]
-    float kecepatan = 10.5f;
-    // Membuat variable pemain menang dengan tipe bool, dengan nilai awal "false"
-    [SerializeField]
-    bool pemainMenang = false;
-    // Membuat variable posisi 2D dengan tipe Vector2, dengan nilai awal "(x: 0, y:0)"
-    [SerializeField]
-    Vector2 posisi2D = new Vector3(0, 0);
-    // Membuat variable posisi 3D dengan tipe Vector3, dengan nilai awal "(x: 0, y:0, z:0)"
-    [SerializeField]
-    Vector3 posisi3D = new Vector3(0, 0, 0);
-
-    // Membuat variable kecepatan dengan tipe integer, dengan nilai awal "100"
-    int score = 100;
-
-    void Start()
+    private void Awake()
     {
-        // Memanggil function MunculkanLog(), 
-        // untuk memunculkan log dengan nilai variable nama yaitu Agate
-        MunculkanNama();
-
-        // Memanggil function TambahAngka(angka1: 5, angka2: 3), 
-        // untuk memunculkan log dengan nilai variable hasil tambah 5 + 3 = 8
-        TambahAngka(5, 3);
+        Debug.Log("Awake");
     }
 
-    void Update()
+    private void OnEnable()
     {
-
+        Debug.Log("On Enable");
     }
 
-    // Membuat function untuk memunculkan log dengan nilai variable nama
-    void MunculkanNama()
+    private void Start()
     {
-        // Memunculkan log dengan nilai variable nama yaitu Agate
-        Debug.Log(nama);
+        Debug.Log("Start");
     }
 
-    // Membuat function untuk menambahkan angka
-    void TambahAngka(int angka1, int angka2)
+    private void Update()
     {
-        // Membuat variable hasil, dengan nilai nya adalah hasil tambah angka1 dan angka2
-        int hasil = angka1 + angka2;
-        // Memunculkan log dengan nilai variable hasil
-        Debug.Log(hasil);
+        Debug.Log("Update");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Late Update");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("On Disable");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("On Destroy");
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("On Application Quit");
     }
 }
