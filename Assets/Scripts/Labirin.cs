e36808c8492db284f6d46e1b31e91a2af9c50731
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 12:04:13 +0700

    mendeteksi input

--- Assets/Scripts/Labirin.cs
+++ Assets/Scripts/Labirin.cs
@@ -18,6 +18,28 @@ public class Labirin : MonoBehaviour
 
     void Update()
     {
+        // Mendeteksi input ketika spasi mulai ditekan
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Kode di dalam blok if ini akan dijalankan ketika spasi mulai ditekan
+            Debug.Log("Spasi mulai ditekan");
+        }
+
+        // Mendeteksi input selama spasi ditekan
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Kode di dalam blok if ini akan dijalankan selama spasi ditekan
+            Debug.Log("Spasi ditekan");
+
+        }
+
+        // Mendeteksi input ketika spasi dilepas
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // Kode di dalam blok if ini akan dijalankan ketika spasi dilepas
+            Debug.Log("Spasi dilepas");
+        }
+
         // Mendapatkan nilai rotasi sekarang
         // rotasi sekarang dirotasi di sumbu x +1 dengan kecepetan rotasi yang telah ditentukan
         rotasiSekarang = rotasiSekarang + new Vector3(1, 0, 0) * kecepatanRotasi * Time.deltaTime;
