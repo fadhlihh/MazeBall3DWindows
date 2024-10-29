73f5feb1b09eea43e26df00884f15f3cd118fafa
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 11:40:20 +0700

    add variable

--- Assets/Scripts/Labirin.cs
+++ Assets/Scripts/Labirin.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Labirin : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Membuat variable nama dengan tipe string, dengan nilai awal "Agate"
+    [SerializeField]
+    string nama = "Agate";
+    // Membuat variable kecepatan dengan tipe float, dengan nilai awal "10.5"
+    [SerializeField]
+    float kecepatan = 10.5f;
+    // Membuat variable pemain menang dengan tipe bool, dengan nilai awal "false"
+    [SerializeField]
+    bool pemainMenang = false;
+    // Membuat variable posisi 2D dengan tipe Vector2, dengan nilai awal "(x: 0, y:0)"
+    [SerializeField]
+    Vector2 posisi2D = new Vector3(0, 0);
+    // Membuat variable posisi 3D dengan tipe Vector3, dengan nilai awal "(x: 0, y:0, z:0)"
+    [SerializeField]
+    Vector3 posisi3D = new Vector3(0, 0, 0);
+
+    // Membuat variable kecepatan dengan tipe integer, dengan nilai awal "100"
+    int score = 100;
+
     void Start()
     {
-        // Perintah untuk memunculkan log hello di console
-        Debug.Log("Hello");
+        // Memunculkan log dengan nilai variable nama yaitu Agate
+        Debug.Log(nama);
     }
 
-    // Update is called once per frame
     void Update()
     {
 
