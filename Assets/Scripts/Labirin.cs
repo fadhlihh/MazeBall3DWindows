83ac7386c9f079014542f80e4f14e85908cf8e9f
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 11:49:46 +0700

    add access component

--- Assets/Scripts/Labirin.cs
+++ Assets/Scripts/Labirin.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Labirin : MonoBehaviour
 {
-    // Membuat variable untuk direferensikan ke object
+    // Membuat variable untuk direferensikan ke transform
     [SerializeField]
-    GameObject labirin;
+    Transform transformLabirin;
 
     void Start()
     {
-        // Memunculkan nama object yang dimasukkan ke dalam variable labirin
-        Debug.Log(labirin.name);
-        // Memunculkan status active/non-active object yang dimasukkan ke dalam variable labirin
-        Debug.Log(labirin.activeInHierarchy);
+        // Memunculkan posisi object dari component transform labirin
+        Debug.Log(transformLabirin.position);
+        // Memunculkan rotasi object dari component transform labirin
+        Debug.Log(transformLabirin.rotation);
+        // Memunculkan skala object dari component transform labirin
+        Debug.Log(transformLabirin.localScale);
 
-        // Memunculkan nama object yang dimasukkan ke dalam variable labirin menjadi Maze
-        labirin.name = "Maze";
-        // Mengubah status object yang dimasukkan ke dalam variable labirin menjadi aktif
-        labirin.SetActive(true);
+        // Menentukan nilai posisi baru menggunakan variable tipe vector
+        Vector3 posisiBaru = new Vector3(10, 0, 0);
+        // Mengubah posisi dari component transform labirin di titik posisi baru
+        transformLabirin.position = posisiBaru;
+
+        // Menentukan nilai rotasi baru menggunakan variable tipe vector
+        Vector3 rotasiBaru = new Vector3(90, 0, 0);
+        // Mengubah rotasi dari component transform labirin dengan nilai variable rotasi baru
+        // Nilai vector rotasi perlu dikorvensi ke sudut menggunakan Quaternion.Euler
+        transformLabirin.rotation = Quaternion.Euler(rotasiBaru);
+
+        // Menentukan nilai rotasi baru menggunakan variable tipe vector
+        Vector3 skalaBaru = new Vector3(2, 2, 2);
+        // Mengubah skala dari component transform labirin dengan nilai variable skala baru
+        transformLabirin.localScale = skalaBaru;
     }
 }
