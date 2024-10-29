3c5ae3145ca9662ac6e1bd3a943d4855cbe03a98
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 11:37:16 +0700

    add labirin script

---
+++ Assets/Scripts/Labirin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Labirin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Perintah untuk memunculkan log hello di console
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
