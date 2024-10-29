3b792ea2d0709ead478512331e2317bffae0ecb5
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-10-29 11:53:04 +0700

    add challenge 4

--- Assets/Scripts/Labirin.cs
+++ Assets/Scripts/Labirin.cs
@@ -7,30 +7,22 @@ public class Labirin : MonoBehaviour
     // Membuat variable untuk direferensikan ke transform
     [SerializeField]
     Transform transformLabirin;
+    [SerializeField]
+    float kecepatanRotasi;
+
+    Vector3 rotasiSekarang = new Vector3(0, 0, 0);
 
     void Start()
     {
-        // Memunculkan posisi object dari component transform labirin
-        Debug.Log(transformLabirin.position);
-        // Memunculkan rotasi object dari component transform labirin
-        Debug.Log(transformLabirin.rotation);
-        // Memunculkan skala object dari component transform labirin
-        Debug.Log(transformLabirin.localScale);
+    }
 
-        // Menentukan nilai posisi baru menggunakan variable tipe vector
-        Vector3 posisiBaru = new Vector3(10, 0, 0);
-        // Mengubah posisi dari component transform labirin di titik posisi baru
-        transformLabirin.position = posisiBaru;
-
-        // Menentukan nilai rotasi baru menggunakan variable tipe vector
-        Vector3 rotasiBaru = new Vector3(90, 0, 0);
-        // Mengubah rotasi dari component transform labirin dengan nilai variable rotasi baru
+    void Update()
+    {
+        // Mendapatkan nilai rotasi sekarang
+        // rotasi sekarang dirotasi di sumbu x +1 dengan kecepetan rotasi yang telah ditentukan
+        rotasiSekarang = rotasiSekarang + new Vector3(1, 0, 0) * kecepatanRotasi * Time.deltaTime;
+        // Mengubah rotasi dari component transform labirin dengan nilai variable rotasi sekarang
         // Nilai vector rotasi perlu dikorvensi ke sudut menggunakan Quaternion.Euler
-        transformLabirin.rotation = Quaternion.Euler(rotasiBaru);
-
-        // Menentukan nilai rotasi baru menggunakan variable tipe vector
-        Vector3 skalaBaru = new Vector3(2, 2, 2);
-        // Mengubah skala dari component transform labirin dengan nilai variable skala baru
-        transformLabirin.localScale = skalaBaru;
+        transformLabirin.rotation = Quaternion.Euler(rotasiSekarang);
     }
 }
